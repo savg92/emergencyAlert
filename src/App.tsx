7b07dae907483f1ab337066b219551a42b23d521
savg92 <1992savg@gmail.com>
2023-11-06 19:38:42 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+const App = () => {
+
+  return (
+    <>
+      <p className='underline'>Hello world!</p>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+
+const Header = () => {
+  return (
+    <>
+        <header>
+            <nav>
+
+            </nav>
+        </header>
+    </>
+  )
+}
+
+export default Header
