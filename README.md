b81cbf922b753cd36e7ed89d2ca87eca5bc47182
savg92 <1992savg@gmail.com>
2023-11-06 20:13:46 -0300

    Add early warning system for emergencies and update header component

--- README.md
+++ README.md
@@ -1,27 +1,6 @@
-# React + TypeScript + Vite
+# Alerta temprana
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
-
-## Expanding the ESLint configuration
-
-If you are developing a production application, we recommend updating the configuration to enable type aware lint rules:
-
-- Configure the top-level `parserOptions` property like this:
-
-```js
-   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
-    tsconfigRootDir: __dirname,
-   },
-```
-
-- Replace `plugin:@typescript-eslint/recommended` to `plugin:@typescript-eslint/recommended-type-checked` or `plugin:@typescript-eslint/strict-type-checked`
-- Optionally add `plugin:@typescript-eslint/stylistic-type-checked`
-- Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and add `plugin:react/recommended` & `plugin:react/jsx-runtime` to the `extends` list
+## Descripción
+Aplicación para una organización de protección civil que permita dar la alerta
+temprana de emergencias como focos de incendio, inundaciones, ayuda a la comunidad,
+accidentes etc.
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,61 @@
+import { useState } from 'react';
+
 const Header = () => {
+	const [isMobileNavVisible, setIsMobileNavVisible] = useState(false);
+
+	const toggleMobileNav = () => {
+		setIsMobileNavVisible(!isMobileNavVisible);
+	};
+
 	return (
 		<>
-			<header className="header flex justify-between items-center">
-				<div className="logo flex items-center gap-2">
-					<img src="#" alt="Logo image" />
-					<span>Logo</span>
+			<header className="flex justify-between items-center p-5 bg-blue-500">
+				<div className="flex items-center gap-2">
+					<img src="#" alt="Logo image" className="w-10 h-10" />
+					<span className="text-white">Logo</span>
 				</div>
-				<nav className="nav flex gap-4">
+				<nav className="hidden md:flex gap-4">
 					<ul className="flex gap-4">
 						<li>
-							<a href="#">Home</a>
+							<a href="#" className="text-white">Home</a>
 						</li>
 						<li>
-							<a href="#">About</a>
+							<a href="#" className="text-white">About</a>
 						</li>
 						<li>
-							<a href="#">Contact</a>
+							<a href="#" className="text-white">Contact</a>
 						</li>
 					</ul>
 				</nav>
+				<button onClick={toggleMobileNav} className="md:hidden p-2 rounded">
+					<svg
+						className="w-6 h-6 text-white"
+						fill="none"
+						stroke="currentColor"
+						viewBox="0 0 24 24"
+						xmlns="http://www.w3.org/2000/svg">
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth="2"
+							d="M4 6h16M4 12h16M4 18h16"></path>
+					</svg>
+				</button>
+				{isMobileNavVisible && (
+					<nav className="md:hidden">
+						<ul className="flex flex-col gap-2 p-5 bg-blue-500 text-white">
+							<li>
+								<a href="#">Home</a>
+							</li>
+							<li>
+								<a href="#">About</a>
+							</li>
+							<li>
+								<a href="#">Contact</a>
+							</li>
+						</ul>
+					</nav>
+				)}
 			</header>
 		</>
 	);
