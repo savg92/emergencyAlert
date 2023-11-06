d9e6941c5e10c1941d6b1736a0e32763b3fc4e62
savg92 <1992savg@gmail.com>
2023-11-06 20:02:37 -0300

    Add logo and navigation to header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,23 @@
 const Header = () => {
 	return (
 		<>
-			<header>
-				<nav>
+			<header className="header flex justify-between items-center">
+				<div className="logo flex items-center gap-2">
+					<img src="#" alt="Logo image" />
 					<span>Logo</span>
+				</div>
+				<nav className="nav flex gap-4">
+					<ul className="flex gap-4">
+						<li>
+							<a href="#">Home</a>
+						</li>
+						<li>
+							<a href="#">About</a>
+						</li>
+						<li>
+							<a href="#">Contact</a>
+						</li>
+					</ul>
 				</nav>
 			</header>
 		</>
